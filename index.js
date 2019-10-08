fbc4fac41e32bc8e5c9434a99379ced2abefcf08
jonathan stewart <jstewart0788@gmail.com>
2019-10-08 16:34:03 -0400

    fix to api

--- index.js
+++ index.js
@@ -2,28 +2,37 @@ const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 //Middleware
-app
-  .use(logger("dev"))
-  .use(bodyParser.json({limit: '50mb'}))
-  .use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 //Initialize API Routes
-app.post('/temperature', (req, res) => res.send('temperature is:', req.body.temp))
-
+app.post('/temperature', (req, res) => {
+    console.log('TEST', req.body);
+    res.json({ msg: `temperature is: ${req.body.temp}` });
+})
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send(err);
+    // render the error page
+    res.status(err.status || 500);
+    res.send(err);
 });
 
 /**
